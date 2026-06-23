ada310736b789e7de3ee2ceab8a54a0014c8e39b
erikGuerreroBravo <erik.guerrero.bravo@hotmail.com>
2026-06-22 23:14:51 -0600

    fix:empeamos a construir nuestro elemento facade apra el producto.

--- angular-soft/src/app/features/products/application/product-facade.ts
+++ angular-soft/src/app/features/products/application/product-facade.ts
@@ -1,6 +1,23 @@
-import { Service } from '@angular/core';
+import { Injectable, computed, inject, signal } from '@angular/core';
+import { finalize } from 'rxjs';
 
-@Service()
+import { Product } from '../domain/product.model';
+import { ProductApi }  from '../infrastructure/product-api';
+
+@Injectable({
+  providedIn: 'root'
+})
 export class ProductFacade {
-  
+  private readonly productApi = inject(ProductApi);
+  private readonly _loading = signal(false);
+  private readonly _error = signal<string | null>(null);
+  private readonly _success = signal(false);
+  private readonly _createProduct = signal<Product| null>(null);
+
+readonly loading = computed( ()=> this._loading()  );
+readonly error = computed( () => this._error());
+readonly success = computed( () => this._success());
+readonly createdProduct = computed( ()=> this._createProduct());
+ 
+
 }
