f461a2a7dfbbf1f1b5a9a0509625c34cb72f9abd
erikGuerreroBravo <erik.guerrero.bravo@hotmail.com>
2026-06-22 23:28:12 -0600

    fix:terminamos el facede del producto para enviarlo al  servicio y conectarlo con la interfzz grafica del producto.

--- angular-soft/src/app/features/products/application/product-facade.ts
+++ angular-soft/src/app/features/products/application/product-facade.ts
@@ -18,6 +18,31 @@ readonly loading = computed( ()=> this._loading()  );
 readonly error = computed( () => this._error());
 readonly success = computed( () => this._success());
 readonly createdProduct = computed( ()=> this._createProduct());
- 
+
+create(product:Product): void{
+    this._loading.set(true);
+    this._error.set(null);
+    this._success.set(false);
+    this._createProduct.set(null);
+
+    this.productApi.create(product)
+        .pipe(finalize( () => this._loading.set(false) ) )
+        .subscribe({
+            next:(response)=>{
+                this._success.set(true);
+                this._createProduct.set(response.data);
+            },
+        error: ()=> {
+            this._error.set("No se Pudo guardar el producto, Intenta nuevamente.");
+        }
+
+    });
+}
+resetState():void{
+    this._loading.set(false);
+    this._error.set(null);
+    this._success.set(false);
+    this._createProduct.set(null);
+}
 
 }
